d10e8150737f0643c602c3eac6eb2664f766bac6
Хасаншин Ринат <rinat.h@jetstyle.ru>
2019-10-23 19:00:34 +0500

    change tests

--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import LandingPage from '@/components/LandingPage'
+import Home from '@/pages/Home'
 
-describe('LandingPage.vue', () => {
+describe('pages:Home.vue', () => {
   it('should render correct contents', () => {
     const vm = new Vue({
       el: document.createElement('div'),
-      render: h => h(LandingPage)
+      render: h => h(Home)
     }).$mount()
 
     expect(vm.$el.querySelector('.title').textContent).to.contain('Welcome to your new project!')
